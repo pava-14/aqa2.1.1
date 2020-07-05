a6863bd5c8e8094fd422fed8b0438e0bc79e3d58
Alexander Pavlenko <pava@bk.ru>
2020-07-05 15:10:21 +0700

    README.md added

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 ## Краткое описание
 
-1. 
+1. Cоздан gradle проект
+2. Создан тестовый класс
+3. Настроен CI
 
-
-# Задача №2 - Проверка валидации
-
-## Краткое описание
-
-1. 
+## Project [![Build status](https://ci.appveyor.com/api/projects/status/i1089han31pak2a3/branch/master?svg=true)](https://ci.appveyor.com/project/pava-14/aqa2-1-1/branch/master)
--- src/test/java/ru/netology/order/CreditCardOrderTest.java
+++ src/test/java/ru/netology/order/CreditCardOrderTest.java
@@ -7,18 +7,17 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 public class CreditCardOrderTest {
 
     @Test
     public void shouldCreditCardOrder() {
         open("http://localhost:9999");
-
         SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Петров Иван Петрович");
         form.$("[data-test-id=phone] input").setValue("+79099099090");
         form.$("[data-test-id=agreement]").click();
         form.$("button[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
